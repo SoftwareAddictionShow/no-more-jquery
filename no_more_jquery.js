b30db01fe31a46776f4bd09016018fc8cc0740fd
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2017-04-30 22:59:27 -0700

    Added jshint file and replaced all vars with lets.

--- no_more_jquery.js
+++ no_more_jquery.js
@@ -1,12 +1,12 @@
 // Copyright (c) 2016 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
 // This software uses a MIT style license
 // https://github.com/SoftwareAddictionShow/no-more-jquery
-//"use strict";
+"use strict";
 
 // Great website for reasons not to use jquery:
 // http://youmightnotneedjquery.com
 
-var style = document.createElement('style');
+let style = document.createElement('style');
 style.type = 'text/css';
 style.innerHTML = '.hidden { display: none; }';
 document.getElementsByTagName('head')[0].appendChild(style);
@@ -20,15 +20,15 @@ function $all(selector) {
 }
 
 function hide(selector) {
-	var elements = document.querySelectorAll(selector);
-	for (var i=0; i<elements.length; ++i) {
+	let elements = document.querySelectorAll(selector);
+	for (let i=0; i<elements.length; ++i) {
 		elements[i].classList.add('hidden');
 	}
 }
 
 function show(selector) {
-	var elements = document.querySelectorAll(selector);
-	for (var i=0; i<elements.length; ++i) {
+	let elements = document.querySelectorAll(selector);
+	for (let i=0; i<elements.length; ++i) {
 		elements[i].classList.remove('hidden');
 	}
 }
@@ -51,7 +51,7 @@ function httpPost(url, cb, timeout) {
 
 function httpRequest(url, method, cb, timeout) {
 	timeout = timeout || 3000;
-	var xhr = new XMLHttpRequest();
+	let xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function() {
 		if (this.readyState === 4) {
 			cb(this.response, this.status);
@@ -68,13 +68,13 @@ function httpRequest(url, method, cb, timeout) {
 }
 
 // FIXME: Use a unique random number, rather than this global
-var g_anim_counter = 0;
+let g_anim_counter = 0;
 function animateCSS(element, start_fields, end_fields, duration, iteration_count, direction) {
 	iteration_count = iteration_count || 1;
 	direction = direction || 'normal';
-	var anim_name = 'anim_' + (++g_anim_counter);
+	let anim_name = 'anim_' + (++g_anim_counter);
 
-	var style = document.createElement('style');
+	let style = document.createElement('style');
 	style.type = 'text/css';
 	style.innerHTML = "\
 	." + anim_name + " {\
@@ -109,21 +109,21 @@ function animateCSS(element, start_fields, end_fields, duration, iteration_count
 }
 
 function animateValue(cb, old_value, new_value, target_time) {
-	var is_bigger = old_value > new_value;
-	var diff_value = is_bigger ? old_value - new_value : new_value - old_value;
-	var start_time = null;
+	let is_bigger = old_value > new_value;
+	let diff_value = is_bigger ? old_value - new_value : new_value - old_value;
+	let start_time = null;
 
-	var stepTime = function(timestamp) {
+	let stepTime = function(timestamp) {
 		if (start_time === null) {
 			start_time = timestamp;
 		}
-		var elapsed_time = timestamp - start_time;
-		var percent = elapsed_time / target_time;
+		let elapsed_time = timestamp - start_time;
+		let percent = elapsed_time / target_time;
 		if (percent >= 1.0) {
 			percent = 1.0;
 		}
 
-		var trans_value = 0;
+		let trans_value = 0;
 		if (is_bigger) {
 			trans_value = old_value - (diff_value * percent);
 		} else {
